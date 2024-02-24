56b79c3e72853302c1805064e3bfaf884f047d68
Armando Jr <elfiservice@hotmail.com>
2024-02-24 10:29:16 -0300

    Add initial data to UsersInMemoryRepository

--- Plugins/ThreadsApp.Plugins.DataStore.InMemory/UsersInMemoryRepository.cs
+++ Plugins/ThreadsApp.Plugins.DataStore.InMemory/UsersInMemoryRepository.cs
@@ -5,8 +5,36 @@ namespace ThreadsApp.Plugins.DataStore.InMemory;
 
 public class UsersInMemoryRepository : IUserRepository
 {
+    private static AUser[] _users;
+
+    public UsersInMemoryRepository()
+    {
+        _users = new[]
+        {
+            new AUser
+            {
+                UserName = "jamesmontemagno",
+                DisplayName = "James Montemagno",
+                IsVerified = true,
+                IsFollowing = false,
+                Image = "profilecircle.png",
+                FollowerCount = 1500
+            },
+            new AUser
+            {
+                UserName = "shanselman",
+                DisplayName = "Scott Hanselman",
+                IsVerified = false,
+                IsFollowing = true,
+                HasSimilarFollowers = true,
+                Image = "profilecircle.png",
+                FollowerCount = 1500000
+            }
+        };
+    }
+
     public Task<IEnumerable<AUser>> GetUsersAsync()
     {
-        throw new NotImplementedException();
+        return Task.FromResult<IEnumerable<AUser>>(_users);
     }
 }
