752a6e0f021def19d93095498b3bc33fcf258020
Armando Jr <elfiservice@hotmail.com>
2024-02-24 11:17:39 -0300

    Add ExecuteAsync method to IViewUsersUseCase

--- ThreadsApp.UseCases/Interfaces/IViewUsersUseCase.cs
+++ ThreadsApp.UseCases/Interfaces/IViewUsersUseCase.cs
@@ -1,6 +1,8 @@
-﻿namespace ThreadsApp.UseCases;
+﻿using ThreadsApp.CoreBusiness;
+
+namespace ThreadsApp.UseCases;
 
 public interface IViewUsersUseCase
 {
-
+    Task<IEnumerable<AUser>> ExecuteAsync();
 }
