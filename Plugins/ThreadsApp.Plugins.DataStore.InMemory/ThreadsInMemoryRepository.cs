18c723bd67a6cb7ee4276e59941975bacc67353d
Armando Jr <elfiservice@hotmail.com>
2024-02-24 10:07:36 -0300

    Add ThreadsInMemoryRepository implementation

---
+++ Plugins/ThreadsApp.Plugins.DataStore.InMemory/ThreadsInMemoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ThreadsApp.CoreBusiness;
+using ThreadsApp.UseCases;
+
+namespace ThreadsApp.Plugins.DataStore.InMemory;
+
+// here the Concrete implementation of the IThreadRepository from UseCases layer
+public class ThreadsInMemoryRepository : IThreadRepository
+{
+    public Task<IEnumerable<AThread>> GetThreadsAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
