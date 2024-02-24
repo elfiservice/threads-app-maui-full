23c74ce0cb37fce7a5ce08883901477fc6ec420b
Armando Jr <elfiservice@hotmail.com>
2024-02-24 10:26:28 -0300

    Add UsersInMemoryRepository class

---
+++ Plugins/ThreadsApp.Plugins.DataStore.InMemory/UsersInMemoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using ThreadsApp.CoreBusiness;
+using ThreadsApp.UseCases;
+
+namespace ThreadsApp.Plugins.DataStore.InMemory;
+
+public class UsersInMemoryRepository : IUserRepository
+{
+    public Task<IEnumerable<AUser>> GetUsersAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
